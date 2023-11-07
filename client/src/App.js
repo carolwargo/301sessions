a6c50f69d6ad4372d195e2e613d9bf897653ed99
Carol Wargo <Carolwargo@gmail.com>
2023-11-07 09:13:29 -0500

    added react app

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "../src/views/Home.jsx";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" component={Home}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
