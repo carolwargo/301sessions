6387e777b0b20327ba8e5eeef1d8cbd7866f789b
Carol Wargo <Carolwargo@gmail.com>
2023-11-10 07:47:02 -0500

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Routes, Route, BrowserRouter} from "react-router-dom";
 import Home from "../src/views/Home.jsx";
 import Sessions from './views/Sessions.jsx';
 import Footer from "./components/Footer/index.jsx";
@@ -8,14 +8,14 @@ import Header from './components/Header/index.jsx';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
 <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sessions" element={<Sessions />} />
         </Routes>
         <Footer />
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
